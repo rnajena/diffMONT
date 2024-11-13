ed9b5c725065c2f3d6ca82eee62a2ea6f4b91cb4
Daria <daria-meyer@web.de>
2024-11-13 15:12:42 +0100

    Create README.sh

---
+++ paper_data/README.sh
@@ -0,0 +1,10 @@
+This folder contains result files from the publication ### TODO ###.
+
+## RRMS dataset
+diffONT was compared against metilene and dss based on the RRMS dataset published from Oxford Nanopore Technologies (ONT).
+The ONT R9.4.1 dataset of COLO829 and COLO829BL is available from ONT Open Data Registry https://registry.opendata.aws/ont-open-data/ under the following AWS storage bucket: s3://ont-open-data/rrms_2022.07/, which can be accessed as described by ONT under https://labs.epi2me.io/rrms2022.07/.
+The result files for diffONT, metilene and dss are stored in the corresponding folders.
+
+## GIAB dataset
+diffONT was compared against pycoMeth on the samples HG003 and HG004 from the GIAB consortium.
+The GIAB data can be retrieved via ftp from NCBI https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/analysis/10XGenomics_ChromiumGenome_LongRanger2.0_06202016/.
