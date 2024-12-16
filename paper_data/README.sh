61a51170e2f8cecac8c1d244821c1438c90e334f
Daria <daria-meyer@web.de>
2024-12-16 15:56:40 +0100

    Update README.sh

--- paper_data/README.sh
+++ paper_data/README.sh
@@ -1,10 +1,12 @@
-This folder contains result files from the publication ### TODO ###.
+This folder contains result files from the publication 
+"diffONT: predicting methylation-specific PCR biomarkers based on nanopore sequencing data for clinical application."
 
 ## RRMS dataset
 diffONT was compared against metilene and dss based on the RRMS dataset published from Oxford Nanopore Technologies (ONT).
 The ONT R9.4.1 dataset of COLO829 and COLO829BL is available from ONT Open Data Registry https://registry.opendata.aws/ont-open-data/ under the following AWS storage bucket: s3://ont-open-data/rrms_2022.07/, which can be accessed as described by ONT under https://labs.epi2me.io/rrms2022.07/.
-The result files for diffONT, metilene and dss are stored in the corresponding folders.
+The result files for diffONT are stored in the corresponding folders.
 
 ## GIAB dataset
 diffONT was compared against pycoMeth on the samples HG003 and HG004 from the GIAB consortium.
 The GIAB data can be retrieved via ftp from NCBI https://ftp-trace.ncbi.nlm.nih.gov/giab/ftp/data/AshkenazimTrio/analysis/10XGenomics_ChromiumGenome_LongRanger2.0_06202016/.
+The result files for diffONT are stored in the corresponding folder.
